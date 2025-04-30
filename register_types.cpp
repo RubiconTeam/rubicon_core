b263ec16448506daf725b53d74466640e2e54825
Binpuki <30415792+Binpuki@users.noreply.github.com>
2025-04-29 22:24:47 -0500

    Move core elements into its own submodule

---
+++ register_types.cpp
@@ -0,0 +1,33 @@
+#include "register_types.h"
+
+#include "core/config/engine.h"
+#include "core/object/class_db.h"
+#include "rubicon_core/conductor.h"
+#include "rubicon_core/chart/time_change.h"
+
+static Conductor *ConductorPtr;
+
+void initialize_rubicon_module(ModuleInitializationLevel p_level) {
+	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE)
+		return;
+
+	// Register classes
+
+	// Rubicon.Core
+    GDREGISTER_CLASS(Conductor);
+
+	// Rubicon.Core.Chart
+	GDREGISTER_CLASS(TimeChange);
+
+	// Initialize singletons
+    ConductorPtr = memnew(Conductor);
+    Engine::get_singleton()->add_singleton(Engine::Singleton("Conductor", Conductor::get_singleton()));   
+}
+
+void uninitialize_rubicon_module(ModuleInitializationLevel p_level) {
+	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE)
+		return;
+
+    Engine::get_singleton()->remove_singleton("Conductor");
+	memdelete(ConductorPtr);
+}
---
+++ register_types.h
@@ -0,0 +1,9 @@
+#ifndef RUBICON_CORE_REGISTER_TYPES_H
+#define RUBICON_CORE_REGISTER_TYPES_H
+
+#include "modules/register_module_types.h"
+
+void initialize_rubicon_core_module(ModuleInitializationLevel level);
+void uninitialize_rubicon_core_module(ModuleInitializationLevel level);
+
+#endif
